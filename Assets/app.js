638cddca3539199c80dca1de559dd6f0468e9a36
Hudson Hancock <hudsonhancock@gmail.com>
2021-07-30 13:44:15 -0400

    Retrieve data and input current day data into document

--- Assets/app.js
+++ Assets/app.js
@@ -1,55 +1,110 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    
+    var iconsUrl = 'https://openweathermap.org/img/w/04d.png'
 
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.077888&lon=-83.561546&appid=b0c3f4f604e973f7f3d93e23a6fba2eb&units=imperial';
-    var dataSet = {};
-    
+
+
+
+    var searchInput = $('#s_input');
+    var getWeatherBtn = $('#search_btn')
+
+
+    getWeatherBtn.on("click", function() {
+        // console.log('hello');   
+        var cityName = searchInput.val();
+        console.log(cityName);
+
+        var nameUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=b0c3f4f604e973f7f3d93e23a6fba2eb&units=imperial&cnt=8';
+
         
-    function getWeather(){ 
-        fetch(requestUrl)
-                .then(function (response) {
-                    if (response.ok){
-                        console.log('SUCCESS');
-                    } else {
-                        console.log('ERROR');
-                    }
-                    return response.json();
-                  })
-                .then(function (data) {
-                    console.log(data);
+        fetch(nameUrl)
+        .then(function (response) {
+            if (!response.ok){
+                throw Error("There's been a boo");
+            }
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            var cityNameText = data.city.name
+            console.log(cityNameText);
+            $('#selected_city').text(cityNameText + ' ');
+            
+            var lat = data.city.coord.lat;
+            console.log(lat);
+            var lon = data.city.coord.lon;
+            console.log(lon);
+            
+            var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=b0c3f4f604e973f7f3d93e23a6fba2eb&units=imperial';
+                    fetch(requestUrl)
+                        .then(function (response) {
+                             if (!response.ok){
+                                throw Error("There's been a boo on ONECALL");
+                            }
+                             return response.json();
+                        })
+                            .then(function(data) {
+                                var date = new Date(data.daily[0].dt * 1000);
+
+                                $('#current_date').text(date.toLocaleString().substr(-21, 9));
+
+                                console.log(data);
+                                $('#temp').text(data.daily[0].temp.day);
+                                $('#wind').text(data.daily[0].wind_speed + ' MPH');
+                                $('#humidity').text(data.daily[0].humidity + ' %');
+                                $('#uv').text(data.daily[0].uvi + '+');
+
+                                $('section.hide').removeClass('hide');
+
+                                
+
+                                
+                        });
+
+                
+
+               
+
+
+            })
+
+    })
+
+
+    
+
+
+
+
+
+
+    
+    // function getWeather(){  
+
+    //     fetch(requestUrl)
+    //             .then(function (response) {
+    //                 if (!response.ok){
+    //                     throw Error("ERROR")
+    //                 } 
+    //                 return response.json();
+    //               })
+    //             .then(function (data) {
+    //                 console.log(data);
+                   
                      
-
-
-
-
-                      });
+    //                   });
     
-                    }
-
-                    var city = 2;
-                    var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-                    var temp = "test";
-                    var windSpeed = "test";
-                    var humidity = "test";
-                    var uvIndex = "test"
-
-
-                    createCard(city, currentDate, temp, windSpeed, humidity, uvIndex);
-
-
-                    function createCard(a,b,c,x,y,z,){
-                          console.log('hello')
-                          $('.current').add('<section class="current"><h1 id="today"><span id="selected_city">' + a + '</span><span id="current_date">' + b + '</span></h1><p>Temp: <span id="temp">' + c +'°F</span></p><p>Wind: <span id="wind">' + x + 'MPH</span></p><p>Humidity: <span id="humidity">' + y + '%</span></p><p>UV Index: <span id="uv">' + z + '</span></p></section>')
-                      }
-
-
+    //                 }
     
+    //                 getWeather();
 
 
 
-                    });
+
+                });
+
+                
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # weather-app
 
 ## Key
+
+## Description
+
+Brain Breaks, a productivity app that helps students and business professionals plow through difficult work by using positive reinforcement to keep the vibes up!
