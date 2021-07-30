10dde2038e57e25c06a153a7b58f9e679f9e5339
Hudson Hancock <hudsonhancock@gmail.com>
2021-07-30 14:31:04 -0400

    Added 5-Day forcast

--- Assets/app.js
+++ Assets/app.js
@@ -21,7 +21,7 @@ $( document ).ready(function() {
         fetch(nameUrl)
         .then(function (response) {
             if (!response.ok){
-                throw Error("There's been a boo");
+                throw Error("There's been a boo boo");
             }
             return response.json();
         })
@@ -52,16 +52,34 @@ $( document ).ready(function() {
                                 $('#current_date').text(date.toLocaleString().substr(-21, 9));
 
                                 console.log(data);
-                                $('#temp').text(data.daily[0].temp.day);
+                                $('#temp').text(data.daily[0].temp.day + '°F');
                                 $('#wind').text(data.daily[0].wind_speed + ' MPH');
                                 $('#humidity').text(data.daily[0].humidity + ' %');
                                 $('#uv').text(data.daily[0].uvi + '+');
 
                                 $('section.hide').removeClass('hide');
 
+                                // console.log(data.daily[2].dt);
+
+
+                                for(i = 1; i < 5; i++){
+                                    var milliseconds = new Date(data.daily[i].dt * 1000);
+                                    
+                                    var futureDate = milliseconds.toLocaleString().substr(-21, 9)
+
+                                    var futureTemp = data.daily[i].temp.day;
+                                    var futureWindSpeed = data.daily[i].wind_speed;
+                                    var futureHumidity = data.daily[i].humidity;
+
+                                    $('.container').append('<div class="card"><h4 id="fd">' + futureDate + '</h4><i class="fas fa-cloud-showers-heavy"></i><p id="ft">' + futureTemp + '°F</p><p id="fw">' + futureWindSpeed + ' MPH</p><p id="fh">' + futureHumidity + '%</p></div>');
+                                }
 
                                 
                         });
+
+                        $('#s_history_btn').on('click', function(){
+                            
+                        })
 
                 
 
